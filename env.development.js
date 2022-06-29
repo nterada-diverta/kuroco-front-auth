762ea52e383094f955c971e425de71c85df7e738
nterada-diverta <nterada@diverta.co.jp>
2022-06-29 15:02:22 +0800

    env更新

--- env.development.js
+++ env.development.js
@@ -1,5 +1,5 @@
 module.exports = {
     META_TITLE: '[DEV]Nuxt Auth',
     ROBOTS: 'noindex',
-    BASE_URL: 'https://dev-nuxt-auth.a.kuroco.app'
+    BASE_URL: 'https://kuroco-terada20220621.g.kuroco.app'
 };
